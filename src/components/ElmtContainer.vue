ff2816b8444a0d1724ffab718161727e99f5d711
xiaolidan00 <764937567@qq.com>
2023-04-11 15:12:23 +0800

    feat 基本距离提示功能

---
+++ src/components/ElmtContainer.vue
@@ -0,0 +1,216 @@
+<template>
+  <div id="elmtContainer" @click.self="cleanActive()">
+    <div
+      :class="['elmt-item', activeElement.id == item.id ? 'active' : '']"
+      v-for="item in elmts"
+      :id="item.id"
+      :style="{
+        left: item.props.x + 'px',
+        top: item.props.y + 'px',
+        width: item.props.width + 'px',
+        height: item.props.height + 'px'
+      }"
+      @click="onActiveElmt(item.id)"
+    ></div>
+
+    <div
+      class="guide-line-h"
+      :style="item.style"
+      :key="'h' + idx"
+      v-for="(item, idx) in guideLineH"
+    >
+      {{ item.value ? item.value + 'px' : '' }}
+    </div>
+    <div
+      class="guide-line-v"
+      :style="item.style"
+      :key="'v' + idx"
+      v-for="(item, idx) in guideLineV"
+    >
+      {{ item.value ? item.value + 'px' : '' }}
+    </div>
+  </div>
+</template>
+<script setup>
+  import interact from 'interactjs';
+  import { ref, onMounted, computed, nextTick } from 'vue';
+  import { getGuideLine, setNearDistance } from '../libs/distanceTipLine';
+  setNearDistance(100);
+  const activeElement = computed(() => {
+    if (active.value) {
+      return elmts.value.find((item) => item.id == active.value);
+    }
+    return {};
+  });
+  const active = ref();
+  const elmts = ref([]);
+  const guideLineH = ref([]);
+  const guideLineV = ref([]);
+  const setElementSize = (rect) => {
+    let elmtIndex = elmts.value.findIndex((item) => item.id == active.value);
+    let item = elmts.value[elmtIndex];
+    item.props = rect;
+    elmts.value.splice(elmtIndex, 1, item);
+  };
+  const onActiveElmt = (id) => {
+    active.value = id;
+
+    createGuideLine();
+  };
+  const createGuideLine = () => {
+    nextTick(() => {
+      let { h, v } = getGuideLine(activeElement.value, elmts.value, '#elmtContainer');
+      guideLineH.value = h;
+      guideLineV.value = v;
+    });
+  };
+  const cleanActive = () => {
+    active.value = '';
+    guideLineH.value = [];
+    guideLineV.value = [];
+  };
+  onMounted(() => {
+    let list = [];
+    let container = document.getElementById('elmtContainer');
+    for (let i = 0; i < 10; i++) {
+      list.push({
+        id: 'el' + i,
+        props: {
+          x: Math.round(Math.random() * container.offsetWidth),
+          y: Math.round(Math.random() * container.offsetHeight),
+          width: 200 + i * 10,
+          height: 300 - i * 10
+        }
+      });
+    }
+    elmts.value = list;
+
+    const position = { x: 0, y: 0 };
+    interact('.elmt-item').draggable({
+      listeners: {
+        start(event) {
+          let dom = event.target;
+          position.x = parseInt(dom.style.left) || 0;
+          position.y = parseInt(dom.style.top) || 0;
+          onActiveElmt(dom.id);
+        },
+        move(event) {
+          let dom = event.target;
+
+          position.x += event.dx;
+          position.y += event.dy;
+          dom.style.left = position.x + 'px';
+          dom.style.top = position.y + 'px';
+
+          let newRect = {
+            x: parseInt(dom.style.left),
+            y: parseInt(dom.style.top),
+            height: parseInt(dom.style.height),
+            width: parseInt(dom.style.width)
+          };
+          setElementSize(newRect);
+          createGuideLine();
+        },
+        end(event) {
+          let dom = event.target;
+
+          let newRect = {
+            x: parseInt(dom.style.left),
+            y: parseInt(dom.style.top),
+            height: parseInt(dom.style.height),
+            width: parseInt(dom.style.width)
+          };
+          setElementSize(newRect);
+          createGuideLine();
+        }
+      }
+    });
+    // const sizePos = {
+    //   x: 0,
+    //   y: 0
+    // };
+    interact('.elmt-item').resizable({
+      edges: {
+        top: false,
+        left: false,
+        bottom: true,
+        right: true
+      },
+      listeners: {
+        start(event) {
+          let dom = event.target;
+          // sizePos.x = parseInt(dom.style.left) || 0;
+          // sizePos.y = parseInt(dom.style.top) || 0;
+          onActiveElmt(dom.id);
+        },
+        move: (event) => {
+          let dom = event.target;
+          console.log(event);
+
+          // sizePos.x += event.dx;
+          // sizePos.y += event.dy;
+          // dom.style.left = `${parseInt(sizePos.x)}px`;
+          // dom.style.top = `${parseInt(sizePos.y)}px`;
+          dom.style.height = `${parseInt(event.rect.height)}px`;
+          dom.style.width = `${parseInt(event.rect.width)}px`;
+          let newRect = {
+            x: parseInt(dom.style.left),
+            y: parseInt(dom.style.top),
+            height: parseInt(event.rect.height),
+            width: parseInt(event.rect.width)
+          };
+          setElementSize(newRect);
+          createGuideLine();
+        },
+        end: (event) => {
+          let dom = event.target;
+          let newRect = {
+            x: parseInt(dom.style.left),
+            y: parseInt(dom.style.top),
+            height: parseInt(dom.style.height),
+            width: parseInt(dom.style.width)
+          };
+          setElementSize(newRect);
+          createGuideLine();
+        }
+      }
+    });
+  });
+</script>
+
+<style scoped>
+  .guide-line-h,
+  .guide-line-v {
+    z-index: 9999;
+    pointer-events: none;
+    display: inline-flex;
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+    font-size: 16px;
+    line-height: 16px;
+    color: rgba(0, 0, 0, 0.5);
+  }
+  .guide-line-h {
+    border-bottom: solid 1px dodgerblue;
+  }
+  .guide-line-v {
+    border-left: solid 1px dodgerblue;
+  }
+  #elmtContainer {
+    height: 100%;
+    width: 100%;
+    left: 0;
+    top: 0;
+    position: absolute;
+  }
+  .elmt-item {
+    border: dashed 1px gray;
+    display: inline-block;
+    position: absolute;
+  }
+  .elmt-item.active {
+    background-color: rgba(30, 144, 255, 0.3);
+    border: dashed 1px rgb(30, 144, 255);
+  }
+</style>
---
+++ src/libs/distanceTipLine.js
@@ -0,0 +1,270 @@
+/**
+ * 检查组件是否在选框范围内
+ * @param {Object} props 组件位置大小数据
+ * @param {Object} rect 范围
+ * @returns {Boolean}
+ */
+export function checkPostion(props, rect) {
+  if (
+    rect.x <= props.x &&
+    rect.y <= props.y &&
+    rect.x + rect.width >= props.x + props.width &&
+    rect.y + rect.height >= props.y + props.height
+  ) {
+    return true;
+  }
+  return false;
+}
+
+//附近距离
+let nearD = 160;
+export function setNearDistance(d) {
+  nearD = d;
+}
+/**
+ * 检查组件是否在选框范围内
+ * @param {Object} props 组件位置大小数据
+ * @returns {Boolean}
+ */
+function checkNear(props, rect) {
+  if (checkPostion(props, rect)) {
+    return true;
+  }
+  if (checkPostion(rect, props)) {
+    return true;
+  }
+  let xD = getXDistance(props, rect);
+  let yD = getYDistance(props, rect);
+  if (Math.min(...xD) <= nearD || Math.min(...yD) <= nearD) {
+    return true;
+  }
+
+  return false;
+}
+function getXDistance(props, rect) {
+  return [
+    Math.abs(rect.x - props.x),
+    Math.abs(rect.x + rect.width - props.x),
+    Math.abs(rect.x - (props.x + props.width)),
+    Math.abs(rect.x + rect.width - (props.x + props.width))
+  ];
+}
+function getYDistance(props, rect) {
+  return [
+    Math.abs(rect.y - props.y),
+    Math.abs(rect.y + rect.height - props.y),
+    Math.abs(rect.y - (props.y + props.height)),
+    Math.abs(rect.y + rect.height - (props.y + props.height))
+  ];
+}
+
+const XLine = {
+  0: (props, rect) => {
+    return Math.min(rect.x, props.x);
+  },
+  1: (props, rect) => {
+    return Math.min(rect.x + rect.width, props.x);
+  },
+  2: (props, rect) => {
+    return Math.min(rect.x, props.x + props.width);
+  },
+  3: (props, rect) => {
+    return Math.min(rect.x + rect.width, props.x + props.width);
+  }
+};
+
+const YLine = {
+  0: (props, rect) => {
+    return Math.min(rect.y, props.y);
+  },
+  1: (props, rect) => {
+    return Math.min(rect.y + rect.height, props.y);
+  },
+  2: (props, rect) => {
+    return Math.min(rect.y, props.y + props.height);
+  },
+  3: (props, rect) => {
+    return Math.min(rect.y + rect.height, props.y + props.height);
+  }
+};
+
+export function getGuideLine(activeElement, elmts, el) {
+  let { x: left, y: top, width, height } = activeElement.props;
+  let container = document.querySelector(el);
+  let screenWidth = container.offsetWidth;
+  let screenHeight = container.offsetHeight;
+  let posH = [
+    //左距离
+    {
+      style: {
+        left: 0,
+        top: top + height * 0.5 - 16 + 'px',
+        width: left + 'px'
+      },
+      value: left
+    },
+    //右距离
+    {
+      style: {
+        right: 0,
+        top: top + height * 0.5 - 16 + 'px',
+        width: screenWidth - (left + width) + 'px'
+      },
+      value: screenWidth - (left + width)
+    },
+    //宽度
+    {
+      style: {
+        left: left + 'px',
+        top: top + height * 0.5 - 16 + 'px',
+        width: width + 'px',
+        justifyContent: 'flex-start',
+        border: 'none',
+        paddingLeft: '10px'
+      },
+      value: height
+    },
+    //上边界
+    {
+      style: {
+        left: 0,
+        top: top + 'px',
+        width: '100%',
+        borderBottom: 'dashed 1px rgb(37, 124, 245)'
+      }
+    },
+    //下边界
+    {
+      style: {
+        left: 0,
+        top: top + height + 'px',
+        width: '100%',
+        borderBottom: 'dashed 1px rgb(37, 124, 245)'
+      }
+    }
+  ];
+  let posV = [
+    //上距离
+    {
+      style: {
+        left: left + width * 0.5 + 'px',
+        top: 0,
+        height: top + 'px'
+      },
+      value: top
+    },
+    //下距离
+    {
+      style: {
+        left: left + width * 0.5 + 'px',
+        bottom: 0,
+        height: screenHeight - (top + height) + 'px'
+      },
+      value: screenHeight - (top + height)
+    },
+    //高度
+    {
+      style: {
+        left: left + width * 0.5 + 'px',
+        top: top + 'px',
+        height: height + 'px',
+        alignItems: 'flex-start',
+        border: 'none',
+
+        paddingTop: '10px'
+      },
+      value: height
+    },
+    //左边界
+    {
+      style: {
+        left: left + 'px',
+        top: 0,
+        height: '100%',
+        borderLeft: 'dashed 1px rgb(37, 124, 245)'
+      }
+    },
+    //右边界
+    {
+      style: {
+        left: left + width + 'px',
+        top: 0,
+        height: '100%',
+        borderLeft: 'dashed 1px rgb(37, 124, 245)'
+      }
+    }
+  ];
+  let nearLineH = [];
+  let nearLineV = [];
+  elmts.forEach((elmt) => {
+    if (elmt.id != activeElement.id && checkNear(elmt.props, activeElement.props)) {
+      //横向距离
+      let xD = getXDistance(elmt.props, activeElement.props);
+
+      let isLeft1 = false;
+      let isLeft2 = false;
+      for (let i = 0; i < xD.length; i++) {
+        let d = xD[i];
+        if (i == 2 && isLeft1) {
+          continue;
+        }
+        if (i == 3 && isLeft2) {
+          continue;
+        }
+        if (d > 0 && d <= nearD) {
+          let s = XLine[i](elmt.props, activeElement.props, d);
+          if (i == 0) {
+            isLeft1 = true;
+          }
+          if (i == 1) {
+            isLeft2 = true;
+          }
+          nearLineH.push({
+            style: {
+              left: s + 'px',
+              top: elmt.props.y + elmt.props.height * 0.5 - 16 + 'px',
+              width: d + 'px'
+            },
+            value: d
+          });
+        }
+      }
+
+      let yD = getYDistance(elmt.props, activeElement.props);
+
+      let isTop1 = false;
+      let isTop2 = false;
+      for (let i = 0; i < yD.length; i++) {
+        let d = yD[i];
+        if (i == 2 && isTop1) {
+          continue;
+        }
+        if (i == 3 && isTop2) {
+          continue;
+        }
+        if (d > 0 && d <= nearD) {
+          let s = YLine[i](elmt.props, activeElement.props, d);
+          if (i == 0) {
+            isTop1 = true;
+          }
+          if (i == 1) {
+            isTop2 = true;
+          }
+          nearLineV.push({
+            style: {
+              left: elmt.props.x + elmt.props.width * 0.5 + 'px',
+              top: s + 'px',
+              height: d + 'px'
+            },
+            value: d
+          });
+        }
+      }
+    }
+  });
+
+  return {
+    h: posH.concat(nearLineH),
+    v: posV.concat(nearLineV)
+  };
+}
